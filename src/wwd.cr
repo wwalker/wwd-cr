8e6c22b412d6955cb7d05dd4c2b658750921eda7
Wayne Walker <wwalker@solid-constructs.com>
2024-07-04 08:48:41 -0500

    spin

--- src/wwd.cr
+++ src/wwd.cr
@@ -97,7 +97,7 @@ def run_server(port : Int32, config_file : String, rows : Int32, columns : Int32
         when "shortcut"
           # Process.new(cl.args,nil,nil,false,true)
           File.write("shortcut.bat", "start /b" + cl.args)
-          Process.new("start", ["/b", cl.args])
+          Process.new("shortcut.bat")
           context.response.status = HTTP::Status::FOUND
           context.response.headers["Location"] = "/page/#{page_num}"
         when "keys"
