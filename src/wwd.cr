a82d741b7241620692b828c28a6a64933bb0e723
Wayne Walker <wwalker@solid-constructs.com>
2024-07-06 12:51:07 -0500

    refactor

--- src/wwd.cr
+++ src/wwd.cr
@@ -43,9 +43,15 @@ def read_config(file)
   config_lines
 end
 
-def render_page(page, title, rows, columns, config_lines)
-  w = ((100 - 1*(rows + 1))/rows).to_i
-  h = ((100 - 1*(columns + 1))/columns).to_i
+class WDWD
+  def initialize(@rows, @columns, @config_file)
+    @config_lines = read_config(@config_file)
+    @os = self.set_os
+  end # initialize
+
+def render_page(page, title)
+  w = ((100 - 1*(@rows + 1))/@rows).to_i
+  h = ((100 - 1*(@columns + 1))/@columns).to_i
   tr_height = "150px"
   image_width = "135px"
   image_height = "135px"
@@ -53,9 +59,9 @@ def render_page(page, title, rows, columns, config_lines)
   renderer = Renderer.new
 
   row_text = ""
-  (1..rows).each do |row|
+  (1..@rows).each do |row|
     row_content = ""
-    (1..columns).each do |column|
+    (1..@columns).each do |column|
       key = "#{page}-#{row}-#{column}"
       c = config_lines[key]
       context = {key: key, label: c.label, action: c.action, w: w, h: h, image: c.image, image_width: image_width, image_height: image_height, url: c.url}
@@ -67,87 +73,109 @@ def render_page(page, title, rows, columns, config_lines)
   renderer.render("deck_html.j2", {page: page, title: title, table_content: row_text, table_width: "1200px"})
 end
 
-def run_server(port : Int32, config_file : String, rows : Int32, columns : Int32)
-  config_lines = read_config(config_file)
+  def run_server(port : Int32)
+    config_lines = read_config(@config_file)
 
-  server = HTTP::Server.new do |context|
-    case context.request.method
-    when "GET"
-      context.response.status = HTTP::Status::OK
-      case context.request.path
-      when "", "/"
-         context.response.print INDEX_HTML
-      when "/start.html"
-         context.response.print START_HTML
-      when /^\/re-read-config$/
-        config_lines = read_config(config_file)
-        context.response.print "Config re-read\n\n"
-      when /^\/page\/(\d+)$/
-        page = $1.to_i
-        STDERR.puts "rendering page #{$1}\n\n"
-        context.response.print render_page(page, "Page: #{page}", rows, columns, read_config(config_file))
-      when /^\/images\/(.*)$/
-        context.response.print File.read("images/#{$1}")
-      when /^\/pressed\/(\d+-\d+-\d+)$/
-        key = $1
-        key =~ /^(\d+)-/
-        page_num = $1.to_i
-        cl = config_lines[key]
-        STDERR.puts "I should execute something for #{key}\n\n"
-        STDERR.puts "action: #{cl.action}\n\n"
-        STDERR.puts "args: <#{cl.args}>\n\n"
+    server = HTTP::Server.new do |context|
+      case context.request.method
+      when "GET"
+        context.response.status = HTTP::Status::OK
+        case context.request.path
+        when "", "/"
+          context.response.print INDEX_HTML
+        when "/start.html"
+          context.response.print START_HTML
+        when /^\/re-read-config$/
+          config_lines = read_config(config_file)
+          context.response.print "Config re-read\n\n"
+        when /^\/page\/(\d+)$/
+          page = $1.to_i
+          STDERR.puts "rendering page #{$1}\n\n"
+          context.response.print render_page(page, "Page: #{page}")
+        when /^\/images\/(.*)$/
+          context.response.print File.read("images/#{$1}")
+        when /^\/pressed\/(\d+-\d+-\d+)$/
+          key = $1
+          key =~ /^(\d+)-/
+          page_num = $1.to_i
+          cl = config_lines[key]
+          STDERR.puts "I should execute something for #{key}\n\n"
+          STDERR.puts "action: #{cl.action}\n\n"
+          STDERR.puts "args: <#{cl.args}>\n\n"
 
-        case cl.action
-        when "shortcut"
-          # Process.new(cl.args,nil,nil,false,true)
-          # File.write("shortcut.bat", "cmd /c " + cl.args)
-          # Process.new("shortcut.bato")
-          Process.new("cmd", ["/c", cl.args])
-          context.response.status = HTTP::Status::FOUND
-          context.response.headers["Location"] = "/page/#{page_num}"
-        when "keys"
-          File.write("oneshot.ahk", "Send " + cl.args)
-          Process.new("C:/Program Files/AutoHotkey/v2/AutoHotkey64.exe", ["oneshot.ahk"])
-          # Process.new("C:/Program Files/AutoHotkey/v1.1.37.02/AutoHotkeyU64.exe", ["oneshot.ahk"])
-          context.response.status = HTTP::Status::FOUND
-          context.response.headers["Location"] = "/page/#{page_num}"
-        when "cmd"
-          args = cl.args.split("|")
-          command = args.shift
-          Process.new(command, args)
-          context.response.status = HTTP::Status::FOUND
-          context.response.headers["Location"] = "/page/#{page_num}"
+          case cl.action
+          when "shortcut"
+            # Process.new(cl.args,nil,nil,false,true)
+            # File.write("shortcut.bat", "cmd /c " + cl.args)
+            # Process.new("shortcut.bato")
+            Process.new("cmd", ["/c", cl.args])
+            context.response.status = HTTP::Status::FOUND
+            context.response.headers["Location"] = "/page/#{page_num}"
+          when "keys"
+            File.write("oneshot.ahk", "Send " + cl.args)
+            Process.new("C:/Program Files/AutoHotkey/v2/AutoHotkey64.exe", ["oneshot.ahk"])
+            # Process.new("C:/Program Files/AutoHotkey/v1.1.37.02/AutoHotkeyU64.exe", ["oneshot.ahk"])
+            context.response.status = HTTP::Status::FOUND
+            context.response.headers["Location"] = "/page/#{page_num}"
+          when "cmd"
+            args = cl.args.split("|")
+            command = args.shift
+            Process.new(command, args)
+            context.response.status = HTTP::Status::FOUND
+            context.response.headers["Location"] = "/page/#{page_num}"
+          else
+            context.response.status = HTTP::Status::BAD_REQUEST
+            context.response.print "Unknown command type #{cl.action}!\n\n"
+          end
         else
           context.response.status = HTTP::Status::BAD_REQUEST
-          context.response.print "Unknown command type #{cl.action}!\n\n"
+          context.response.print "Request path is invalid #{context.request.path}!\n\n"
         end
       else
-        context.response.status = HTTP::Status::BAD_REQUEST
-        context.response.print "Request path is invalid #{context.request.path}!\n\n"
+        context.response.status = HTTP::Status::METHOD_NOT_ALLOWED
+        context.response.headers["Allow"] = "GET, POST"
+        context.response.print "Method Not Allowed\n"
       end
-    else
-      context.response.status = HTTP::Status::METHOD_NOT_ALLOWED
-      context.response.headers["Allow"] = "GET, POST"
-      context.response.print "Method Not Allowed\n"
     end
+
+    address = server.bind_tcp "0.0.0.0", 5657, true
+    puts "Listening on http://#{address}"
+
+    # This call blocks until the process is terminated
+    server.listen
+  end # run_server
+
+  def run
+    run_server(8080)
   end
-
-  address = server.bind_tcp "0.0.0.0", 5657, true
-  puts "Listening on http://#{address}"
-
-  # This call blocks until the process is terminated
-  server.listen
-end
+end # class WDWD
 
 def main
   rows = ARGV[0].to_i
   columns = ARGV[1].to_i
   file = ARGV[2]
 
-  run_server(8080, file, rows, columns)
+  wdwd = WDWD.new(rows, columns, file)
+  wdwd.run
 end
 
 INDEX_HTML = {{ read_file("#{__DIR__}/../files/index.html") }}
 START_HTML = {{ read_file("#{__DIR__}/../files/start.html") }}
 STDERR.puts "Running on Crystal #{Crystal::VERSION} #{Crystal::HOST_TRIPLE}\n\n"
+
+case Crystal::HOST_TRIPLE
+when /windows/
+  STDERR.puts "Running on Windows\n\n"
+  @os = "windows"
+when /linux/
+  STDERR.puts "Running on Linux\n\n"
+  @os = "linux"
+when /darwin/
+  STDERR.puts "Running on macOS\n\n"
+  @os = "macos"
+else
+  STDERR.puts "Unknown host triple #{Crystal::HOST_TRIPLE}"
+  exit 2 # Unknown OS
+end
+
 main()
